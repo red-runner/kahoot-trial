5036da2320559028210d9be5c06a545d9115daab
Pol <redrunner.dev@gmail.com>
2023-02-11 12:48:15 +0100

    feat(header): remove unnecessary props

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,7 @@
-import { StylesWrapper } from "./header.styles";
+import { StylesWrapper } from './header.styles';
 
 interface HeaderProps {
   children: string;
-  onClick?: ({ points, bonus }: { points: number; bonus: number }) => void;
-  points?: number;
-  bonus?: number;
-  backgroundColor: string;
 }
 
 export const Header = ({ children }: HeaderProps) => {
