0acaf1f31ce1d298a7177580ec8f5b66199071d2
Pol <redrunner.dev@gmail.com>
2023-02-11 12:48:47 +0100

    feat(header/styles): align header to primary color style

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -1,13 +1,17 @@
-import styled from "styled-components";
-import { montserratRegularBold } from "../../styles";
+import styled from 'styled-components';
+import { montserratRegularBold } from '../../styles';
 
 export const StylesWrapper = styled.div`
   --box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.2);
 
   ${montserratRegularBold}
 
-  height: 3.75rem;
+  align-items: center;
+  background-color: var(--primary);
+  color: var(--white);
+  display: flex;
+  height: 3rem;
+  padding-left: var(--spacing-l);
   width: 100%;
-  background-color: #888888;
-  margin-left: var(--spacing-l);
+  box-shadow: var(--box-shadow);
 `;
