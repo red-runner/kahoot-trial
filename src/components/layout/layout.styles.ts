9675bfcb9a2a6059d065cac701c8853bc0d7645f
Pol <redrunner.dev@gmail.com>
2023-02-11 16:55:22 +0100

    feat(layout): add box shadow to player-item section

--- src/components/layout/layout.styles.ts
+++ src/components/layout/layout.styles.ts
@@ -13,4 +13,8 @@ export const StylesWrapper = styled.main`
     grid-template-rows: var(--grid-header-height) calc(100vh - var(--grid-header-height));
     grid-template-columns: 100%;
   }
+
+  .game__section--player-items {
+    box-shadow: -5px 0px 10px 0 rgb(0 0 0 / 5%);
+  }
 `;
