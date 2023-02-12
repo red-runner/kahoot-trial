4ca88a14f1c711b170a56d2e2b632f9f97edd0cb
Pol <redrunner.dev@gmail.com>
2023-02-12 21:55:06 +0100

    chore(item): add data-testid

--- src/components/player-items/components/item/item.tsx
+++ src/components/player-items/components/item/item.tsx
@@ -3,18 +3,26 @@ import { ItemProps } from './item.interface';
 import { StylesWrapper } from './item.styles';
 import { Tile } from '../../../tile';
 
-export const Item: FC<ItemProps> = ({ tile, quantity, score }) => {
+export const Item: FC<ItemProps> = ({ tile, quantity = 0, score = 0 }) => {
   return (
-    <StylesWrapper className="player-item" data-testid="player-item">
-      <Tile
-        style={{ '--tile-size': '3rem', '--font-size': '1.5rem' }}
-        className="item__tile"
-        backgroundColor={tile?.backgroundColor}
-      >
-        {tile?.children}
-      </Tile>
-      <span className="item__quantity">{quantity}</span>
-      <span className="item__score">{score}</span>
+    <StylesWrapper className="item" data-testid="item">
+      {tile?.children && (
+        <Tile
+          style={{ '--tile-size': '3rem', '--font-size': '1.5rem' }}
+          className="item__tile"
+          backgroundColor={tile?.backgroundColor}
+        >
+          {tile?.children}
+        </Tile>
+      )}
+
+      <span data-testid="item-quantity" className="item__quantity">
+        {quantity}
+      </span>
+
+      <span data-testid="item-score" className="item__score">
+        {score}
+      </span>
     </StylesWrapper>
   );
 };
