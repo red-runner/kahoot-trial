8d471e0d4c3f5de97ea40848ef0c4682a1c68ab1
Pol <redrunner.dev@gmail.com>
2023-02-12 22:22:23 +0100

    fix(styles): align to convention

--- src/components/player-items/components/items-header/items-header.styles.ts
+++ src/components/player-items/components/items-header/items-header.styles.ts
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 import { montserratRegularBold } from '../../../../styles';
 
 export const StyledWrapper = styled.div<{ gridTemplateColumns: string }>`
-  --grid-height: 2rem;
+  --item-header-grid-height: 2rem;
+  --item-header-background-color: #c19cfc;
 
   ${montserratRegularBold}
-  height: var(--grid-height);
+  height: var(--item-header-grid-height);
   display: grid;
   padding: var(--spacing-s);
-  grid-template-rows: var(--grid-height);
+  grid-template-rows: var(--item-header-grid-height);
   grid-template-columns: ${(props) => props.gridTemplateColumns};
-  background-color: #c19cfc;
+  background-color: var(--item-header-background-color);
   justify-items: center;
   align-items: center;
 `;
--- src/components/tiles-game/tiles-game.styles.ts
+++ src/components/tiles-game/tiles-game.styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { Layout } from '../layout';
 
 export const StylesWrapper = styled(Layout)`
+  --tiles-game-section-box-shadow: -5px 0px 10px 0 rgb(0 0 0 / 5%);
+
   .tiles-game__section {
     display: grid;
     grid-template-rows: var(--grid-header-height) calc(100vh - var(--grid-header-height));
@@ -9,6 +11,6 @@ export const StylesWrapper = styled(Layout)`
   }
 
   .tiles-game__section--player-items {
-    box-shadow: -5px 0px 10px 0 rgb(0 0 0 / 5%);
+    box-shadow: var(--tiles-game-section-box-shadow);
   }
 `;
