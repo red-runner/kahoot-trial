54bb72b7347ea28501d30e6f90dbce2da584841f
Pol <redrunner.dev@gmail.com>
2023-02-12 21:53:45 +0100

    feat(layout): create interface file

--- src/components/layout/layout.styles.ts
+++ src/components/layout/layout.styles.ts
@@ -7,14 +7,4 @@ export const StylesWrapper = styled.main`
   display: grid;
   grid-template-columns: calc(100vw - var(--grid-sidebar-width)) var(--grid-sidebar-width);
   grid-template-rows: 100vh;
-
-  .game__section {
-    display: grid;
-    grid-template-rows: var(--grid-header-height) calc(100vh - var(--grid-header-height));
-    grid-template-columns: 100%;
-  }
-
-  .game__section--player-items {
-    box-shadow: -5px 0px 10px 0 rgb(0 0 0 / 5%);
-  }
 `;
