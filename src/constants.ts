990567f891e16c0acf9d425d07c47d1d2168bdfa
Pol <redrunner.dev@gmail.com>
2023-02-12 16:10:47 +0100

    feat(constants): expose project constants

---
+++ src/constants.ts
@@ -0,0 +1,32 @@
+export const TILES = [
+  {
+    children: 'A',
+    backgroundColor: 'red',
+    points: 50,
+    bonus: {
+      points: 200,
+      multiple: 3
+    }
+  },
+  {
+    children: 'B',
+    backgroundColor: 'blue',
+    points: 30,
+    bonus: {
+      points: 90,
+      multiple: 2
+    }
+  },
+  {
+    children: 'C',
+    backgroundColor: 'purple',
+    points: 20
+  },
+  {
+    children: 'D',
+    backgroundColor: 'green',
+    points: 15
+  }
+];
+
+export const NEW_GAME = 'NEW GAME';
