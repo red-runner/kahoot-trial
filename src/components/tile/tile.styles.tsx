6b366195f7be2e650c6a84898eb16da44697d15c
Pol <redrunner.dev@gmail.com>
2023-02-12 21:51:54 +0100

    feat(tile.styles): add backgroundColor fallback

--- src/components/tile/tile.styles.tsx
+++ src/components/tile/tile.styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { montserratRegularDisplay } from '../../styles';
 
-export const StylesWrapper = styled.div<{ backgroundColor: string; role?: string }>`
+export const StylesWrapper = styled.div<{ backgroundColor?: string; role?: string }>`
   --tile-size: 16rem;
   --tile-box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.2);
 
@@ -9,7 +9,7 @@ export const StylesWrapper = styled.div<{ backgroundColor: string; role?: string
 
   height: var(--tile-size);
   width: var(--tile-size);
-  background-color: ${(props) => props.backgroundColor};
+  background-color: ${(props) => (props.backgroundColor ? props.backgroundColor : 'var(--primary)')};
   display: flex;
   align-items: center;
   justify-content: center;
