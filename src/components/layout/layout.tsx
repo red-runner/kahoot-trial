e3508ef373cc24a9e45ec02c8a54ca0302325730
Pol <redrunner.dev@gmail.com>
2023-02-12 21:54:48 +0100

    feat(layout): add children conditional

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,12 @@
-import { ReactNode, FC } from 'react';
+import { FC } from 'react';
 import { StylesWrapper } from './layout.styles';
-
-interface LayoutProps {
-  children: ReactNode;
-  className?: string;
-}
+import { LayoutProps } from './layout.interface';
 
 export const Layout: FC<LayoutProps> = ({ children, className }) => {
+  if (!children) {
+    return null;
+  }
+
   return (
     <StylesWrapper className={className} data-testid="layout">
       {children}
