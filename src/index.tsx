6f00f759bbc6bc2868f62a3265a037dd89f5cd69
Pol <redrunner.dev@gmail.com>
2023-02-12 16:11:21 +0100

    chore(index): remove eslint warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import { App } from './App';
 import { GlobalStyles } from './styles';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <GlobalStyles />
